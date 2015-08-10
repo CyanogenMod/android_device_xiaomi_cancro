79706e4a42a66fae35c01fd29e90815536aef002
jrizzoli <joey@cyanogenmoditalia.it>
2015-08-10 10:57:20 +0200

    cancro: update props
    Change-Id: I08996cbe6761e3d1818e49f91ecb56147fbc31d6
    Signed-off-by: jrizzoli <joey@cyanogenmoditalia.it>

--- cancro.mk
+++ cancro.mk
@@ -365,6 +365,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.timed.enable=true \
     persist.debug.wfd.enable=1 \
     persist.sys.wfd.virtual=0 \
+    persist.sys.media.use-awesome=true \
     debug.mdpcomp.4k2kSplit=1
 
 # Zip
