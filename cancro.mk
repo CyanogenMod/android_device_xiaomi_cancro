4eba3d6c6b3ff487fd749d15473b25a3db97f5b5
jrizzoli <joey@cyanogenmoditalia.it>
2015-08-10 14:26:45 +0200

    cancro: set right props for sensors
    Change-Id: I0822ac9f6aebea91da88230f942346c4e8d68191
    Signed-off-by: jrizzoli <joey@cyanogenmoditalia.it>

--- cancro.mk
+++ cancro.mk
@@ -256,20 +256,20 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 #Enable more sensor
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.qti.sensors.qmd=true \
-    ro.qti.sensors.smd=true \
-    ro.qti.sensors.cmc=true \
-    ro.qti.sensors.vmd=true \
-    ro.qti.sensors.gtap=true \
-    ro.qti.sensors.pedometer=true \
-    ro.qti.sensors.pam=true \
-    ro.qti.sensors.scrn_ortn=true \
-    ro.qti.sensors.georv=true \
-    ro.qti.sensors.game_rv=true \
-    ro.qti.sensors.step_detector=true \
-    ro.qti.sensors.step_counter=true \
-    ro.qti.sensors.max_geomag_rotv=60 \
-    persist.debug.sensors.hal=w \
+    ro.qualcomm.sensors.qmd=true \
+    ro.qualcomm.sensors.smd=true \
+    ro.qualcomm.sensors.cmc=true \
+    ro.qualcomm.sensors.vmd=true \
+    ro.qualcomm.sensors.gtap=true \
+    ro.qualcomm.sensors.pedometer=true \
+    ro.qualcomm.sensors.pam=true \
+    ro.qualcomm.sensors.scrn_ortn=true \
+    ro.qualcomm.sensors.georv=true \
+    ro.qualcomm.sensors.game_rv=true \
+    ro.qc.sensors.step_detector=true \
+    ro.qc.sensors.step_counter=true \
+    ro.qc.sensors.max_geomag_rotvec=true \
+    debug.qualcomm.sns.hal=w \
     debug.qualcomm.sns.daemon=w \
     debug.qualcomm.sns.libsensor1=w
 
