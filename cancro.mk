d4834e7a7046776103baeb6cb2a3081bac7d0882
linuxx <joey@cyanogenmoditalia.it>
2015-09-13 22:18:37 +0200

    cancro: move keylayout to device tree [1/2]
    Change-Id: I71004ede12ea926dc312d99bd17b51360a5656e7
    Signed-off-by: linuxx <joey@cyanogenmoditalia.it>

--- cancro.mk
+++ cancro.mk
@@ -127,6 +127,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.ril.enable.amr.wideband=1
 
+# KeyLayout
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/keylayout/atmel-maxtouch.kl:system/usr/keylayout/atmel-maxtouch.kl \
+    $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
+    $(LOCAL_PATH)/keylayout/msm8974-taiko-mtp-snd-card_Button_Jack.kl:system/usr/keylayout/msm8974-taiko-mtp-snd-card_Button_Jack.kl \
+    $(LOCAL_PATH)/keylayout/synaptics_rmi4_i2c.kl:system/usr/keylayout/synaptics_rmi4_i2c.kl
 
 # SoftAP
 PRODUCT_PACKAGES += \
