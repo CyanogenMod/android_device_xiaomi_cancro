4b6ad97f6229b37a31b47957979ce17145124b3e
jrizzoli <joey@cyanogenmoditalia.it>
2015-09-19 15:13:05 +0200

    cancro: don't use ttyHSL0 for console
    Change-Id: Ibb35a2a185b3c170bba3a4b44fc2bb6a1f88ffb5
    Signed-off-by: jrizzoli <joey@cyanogenmoditalia.it>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -47,7 +47,7 @@ COMMON_GLOBAL_CPPFLAGS += -DNO_SECURE_DISCARD
 
 # Kernel
 BOARD_CUSTOM_BOOTIMG_MK := $(CANCRO_PATH)/mkbootimg.mk
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 vmalloc=340M androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3
+BOARD_KERNEL_CMDLINE := console=none vmalloc=340M androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3
 BOARD_KERNEL_SEPARATED_DT := true
 BOARD_KERNEL_BASE        := 0x00000000
 BOARD_KERNEL_PAGESIZE    := 2048
