9cd38e89c6d57c0903c1d58cd52e0d83388d7ced
jrizzoli <joey@cyanogenmoditalia.it>
2015-12-24 14:14:47 +0100

    cancro: update wcnss_xiaomi_client
    Change-Id: Ib7f75dcd3594434d833011c57464c8785f90dedf
    Signed-off-by: jrizzoli <joey@cyanogenmoditalia.it>

--- wifi/Android.mk
+++ wifi/Android.mk
@@ -44,4 +44,11 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/hostapd
 LOCAL_SRC_FILES    := hostapd.deny
 include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE           := libwcnss_qmi
+LOCAL_MODULE_TAGS      := optional
+LOCAL_C_INCLUDES       += hardware/qcom/wlan/wcnss_service
+LOCAL_SRC_FILES        := wcnss_xiaomi_client.c
+LOCAL_CFLAGS           += -Wall
+LOCAL_SHARED_LIBRARIES := libc libcutils libutils liblog libqminvapi
 endif
