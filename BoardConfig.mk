3e11caa1826e9746bc92fe67a4b3376877620f56
jrizzoli <joey@cyanogenmoditalia.it>
2016-01-03 23:18:10 +0100

    cancro: sepolicy: custom domain for gsiff_daemon
    Change-Id: I5fe4abf3ce32b810dfa30e1d7472e2ea3850fb7a
    Signed-off-by: jrizzoli <joey@cyanogenmoditalia.it>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -44,7 +44,7 @@ COMMON_GLOBAL_CFLAGS   += -D__ARM_USE_PLD -D__ARM_CACHE_LINE_SIZE=64 -DNO_SECURE
 COMMON_GLOBAL_CPPFLAGS += -DNO_SECURE_DISCARD -DUSE_RIL_VERSION_10
 
 # Kernel
-BOARD_KERNEL_CMDLINE               := console=none vmalloc=340M androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3
+BOARD_KERNEL_CMDLINE               := console=none vmalloc=340M androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 andorid
 BOARD_KERNEL_SEPARATED_DT          := true
 BOARD_KERNEL_BASE                  := 0x00000000
 BOARD_KERNEL_PAGESIZE              := 2048
