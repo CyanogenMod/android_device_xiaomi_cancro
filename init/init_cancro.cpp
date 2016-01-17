47ce3323bb9a98401535f8d2d108921e83242879
jrizzoli <joey@cyanogenmoditalia.it>
2016-01-17 18:01:00 +0100

    cancro: set ro.product.device
    this has been accidentally removed during m bringup
    
    Change-Id: I38ead4a219225fb9dadf66b7e82a0a89debd52f1
    Signed-off-by: jrizzoli <joey@cyanogenmoditalia.it>

--- init/init_cancro.cpp
+++ init/init_cancro.cpp
@@ -80,6 +80,8 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
         raw_id = strtoul(tmp, NULL, 0);
     }
 
+    property_set("ro.product.device", "cancro");
+
     switch (raw_id) {
         case 1978:
             property_set("ro.product.model", "MI 3W");
