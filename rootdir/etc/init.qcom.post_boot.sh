df06013732089de1594a1febef98a7b8a7a2c15d
Joey Rizzoli <joey@cyanogenmoditalia.it>
2016-05-29 14:22:34 +0200

    cancro: rootdir: fix thermal paths
    Change-Id: I1f7a152aa16d8eca17c552e11b65b78409fc584c
    Signed-off-by: Joey Rizzoli <joey@cyanogenmoditalia.it>

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -58,7 +58,7 @@ case "$target" in
         echo 1 > /sys/module/msm_pm/modes/cpu1/retention/idle_enabled
         echo 1 > /sys/module/msm_pm/modes/cpu2/retention/idle_enabled
         echo 1 > /sys/module/msm_pm/modes/cpu3/retention/idle_enabled
-        echo 1 > /sys/module/msm_thermal/core_control/enabled
+        echo 1 > /sys/kernel/msm_thermal/enabled
         echo Y > /sys/module/clock_krait_8974/parameters/boost
         stop mpdecision
         echo 1 > /sys/devices/system/cpu/cpu1/online
@@ -110,7 +110,7 @@ case "$target" in
                 echo 80 > /sys/devices/system/cpu/cpufreq/ondemand/up_threshold_any_cpu_load
             ;;
         esac
-        echo 1 > /sys/module/msm_thermal/core_control/enabled
+        echo 1 > /sys/kernel/msm_thermal/enabled
         chown -h root.system /sys/devices/system/cpu/mfreq
         chmod -h 220 /sys/devices/system/cpu/mfreq
         chown -h root.system /sys/devices/system/cpu/cpu1/online
