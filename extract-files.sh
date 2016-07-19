d0e8f213e8ab172a930e1a94865a40f6f0ef2216
Steve Kondik <steve@cyngn.com>
2016-07-19 16:40:19 +0200

    cancro: Update how we generate our blob lists
     * Use the new generic extract utils in vendor/cm!
    
    Change-Id: I82ad1ebc3e4e9f7b5084eeeb35d5cfc53938fea6

--- extract-files.sh
+++ extract-files.sh
@@ -1,57 +1,57 @@
 #!/bin/bash
+#
+# Copyright (C) 2016 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
 set -e
 
-export VENDOR=xiaomi
-export DEVICE=cancro
+DEVICE=cancro
+VENDOR=xiaomi
 
-while getopts ":hd:" options
-do
-    case $options in
-        d ) LDIR=$OPTARG ;;
-        h ) echo "Usage: `basename $0` [OPTIONS] "
-            echo "  -d  Fetch blobs from local directory"
-            echo "  -h  Show this help"
-            exit ;;
-        * ) ;;
-    esac
-done
+# Load extractutils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-function extract() {
-    for FILE in `egrep -v '(^#|^$)' $1`; do
-        OLDIFS=$IFS IFS=":" PARSING_ARRAY=($FILE) IFS=$OLDIFS
-        FILE=`echo ${PARSING_ARRAY[0]} | sed -e "s/^-//g"`
-        DEST=${PARSING_ARRAY[1]}
-        if [ -z $DEST ]; then
-            DEST=$FILE
-        fi
-        DIR=`dirname $DEST`
-        if [ ! -d $2/$DIR ]; then
-            mkdir -p $2/$DIR
-        fi
-        if [ -z $LDIR ]; then
-            # Try CM target first
-            adb pull /system/$DEST $2/$DEST
-            # if file does not exist try OEM target
-            if [ "$?" != "0" ]; then
-                adb pull /system/$FILE $2/$DEST
-            fi
-        else
-            # Try CM target first
-            cp $LDIR/system/$DEST $2/$DEST
-            # if file does not exist try OEM target
-            if [ "$?" != "0" ]; then
-                cp $LDIR/system/$FILE $2/$DEST
-            fi
-        fi
-    done
-}
+CM_ROOT="$MY_DIR"/../../..
 
+HELPER="$CM_ROOT"/vendor/cm/build/tools/extract_utils.sh
+if [ ! -f "$HELPER" ]; then
+    echo "Unable to find helper script at $HELPER"
+    exit 1
+fi
+. "$HELPER"
 
+if [ $# -eq 0 ]; then
+  SRC=adb
+else
+  if [ $# -eq 1 ]; then
+    SRC=$1
+  else
+    echo "$0: bad number of arguments"
+    echo ""
+    echo "usage: $0 [PATH_TO_EXPANDED_ROM]"
+    echo ""
+    echo "If PATH_TO_EXPANDED_ROM is not specified, blobs will be extracted from"
+    echo "the device using adb pull."
+    exit 1
+  fi
+fi
 
-DEVBASE=../../../vendor/$VENDOR/$DEVICE/proprietary
-rm -rf $DEVBASE/*
+# Initialize the helper
+setup_vendor "$DEVICE" "$VENDOR" "$CM_ROOT"
 
-extract ../../$VENDOR/$DEVICE/device-proprietary-files.txt $DEVBASE
+extract "$MY_DIR"/proprietary-files.txt "$SRC"
 
-./../../$VENDOR/$DEVICE/setup-makefiles.sh
+"$MY_DIR"/setup-makefiles.sh
