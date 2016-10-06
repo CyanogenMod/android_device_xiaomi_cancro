4b096315dfcb6301bc827ee83e11abbc7aa1efa2
Joey Rizzoli <joey@cyanogenmoditalia.it>
2016-10-06 14:23:36 +0200

    cancro: bluetooth: update bdroid buildcfg
    Change-Id: I9393c8fbbf752688ae6c3d92671e0d521c9452bb
    Signed-off-by: Joey Rizzoli <joey@cyanogenmoditalia.it>

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -26,8 +26,12 @@
 
 #define BTM_DEF_LOCAL_NAME   "Xiaomi MI3W"
 // Enables Interleave scan
+#define BTA_SKIP_BLE_READ_REMOTE_FEAT TRUE
 #define BTA_HOST_INTERLEAVE_SEARCH  TRUE
-#define MAX_L2CAP_CHANNELS    14
+#define MAX_L2CAP_CHANNELS    16
+// skips conn update at conn completion
+#define BTA_BLE_SKIP_CONN_UPD  TRUE
+#define BT_CLEAN_TURN_ON_DISABLED 1
 
 /* Defined if the kernel does not have support for CLOCK_BOOTTIME_ALARM */
 #define KERNEL_MISSING_CLOCK_BOOTTIME_ALARM TRUE
